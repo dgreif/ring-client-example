7df72f9691ad51114a5968fc7dd0da5fd74363a1
dgreif <dustin.greif@gmail.com>
2022-08-04 07:48:05 -0700

    Update to latest ring client version

--- example.ts
+++ example.ts
@@ -1,98 +1,96 @@
-import 'dotenv/config'
-import { RingApi } from 'ring-client-api'
-import { readFile, writeFile } from 'fs'
-import { promisify } from 'util'
+import "dotenv/config";
+import { RingApi } from "ring-client-api";
+import { readFile, writeFile } from "fs";
+import { promisify } from "util";
 
 async function example() {
   const { env } = process,
     ringApi = new RingApi({
       // This value comes from the .env file
       refreshToken: env.RING_REFRESH_TOKEN!,
-      // Listen for dings and motion events
-      cameraDingsPollingSeconds: 2,
       debug: true,
     }),
     locations = await ringApi.getLocations(),
-    allCameras = await ringApi.getCameras()
+    allCameras = await ringApi.getCameras();
 
   console.log(
     `Found ${locations.length} location(s) with ${allCameras.length} camera(s).`
-  )
+  );
 
   ringApi.onRefreshTokenUpdated.subscribe(
     async ({ newRefreshToken, oldRefreshToken }) => {
-      console.log('Refresh Token Updated: ', newRefreshToken)
+      console.log("Refresh Token Updated: ", newRefreshToken);
 
       // If you are implementing a project that use `ring-client-api`, you should subscribe to onRefreshTokenUpdated and update your config each time it fires an event
       // Here is an example using a .env file for configuration
       if (!oldRefreshToken) {
-        return
+        return;
       }
 
-      const currentConfig = await promisify(readFile)('.env'),
+      const currentConfig = await promisify(readFile)(".env"),
         updatedConfig = currentConfig
           .toString()
-          .replace(oldRefreshToken, newRefreshToken)
+          .replace(oldRefreshToken, newRefreshToken);
 
-      await promisify(writeFile)('.env', updatedConfig)
+      await promisify(writeFile)(".env", updatedConfig);
     }
-  )
+  );
 
   for (const location of locations) {
-    let haveConnected = false
+    let haveConnected = false;
     location.onConnected.subscribe((connected) => {
       if (!haveConnected && !connected) {
-        return
+        return;
       } else if (connected) {
-        haveConnected = true
+        haveConnected = true;
       }
 
-      const status = connected ? 'Connected to' : 'Disconnected from'
-      console.log(`**** ${status} location ${location.name} - ${location.id}`)
-    })
+      const status = connected ? "Connected to" : "Disconnected from";
+      console.log(`**** ${status} location ${location.name} - ${location.id}`);
+    });
   }
 
   for (const location of locations) {
     const cameras = location.cameras,
-      devices = await location.getDevices()
+      devices = await location.getDevices();
 
     console.log(
       `\nLocation ${location.name} (${location.id}) has the following ${cameras.length} camera(s):`
-    )
+    );
 
     for (const camera of cameras) {
-      console.log(`- ${camera.id}: ${camera.name} (${camera.deviceType})`)
+      console.log(`- ${camera.id}: ${camera.name} (${camera.deviceType})`);
     }
 
     console.log(
       `\nLocation ${location.name} (${location.id}) has the following ${devices.length} device(s):`
-    )
+    );
 
     for (const device of devices) {
-      console.log(`- ${device.zid}: ${device.name} (${device.deviceType})`)
+      console.log(`- ${device.zid}: ${device.name} (${device.deviceType})`);
     }
   }
 
   if (allCameras.length) {
     allCameras.forEach((camera) => {
-      camera.onNewDing.subscribe((ding) => {
+      camera.onNewNotification.subscribe(({ ding, subtype }) => {
         const event =
-          ding.kind === 'motion'
-            ? 'Motion detected'
-            : ding.kind === 'ding'
-            ? 'Doorbell pressed'
-            : `Video started (${ding.kind})`
+          ding.detection_type === "motion"
+            ? "Motion detected"
+            : subtype === "ding"
+            ? "Doorbell pressed"
+            : `Video started (${subtype})`;
 
         console.log(
           `${event} on ${camera.name} camera. Ding id ${
-            ding.id_str
+            ding.id
           }.  Received at ${new Date()}`
-        )
-      })
-    })
+        );
+      });
+    });
 
-    console.log('Listening for motion and doorbell presses on your cameras.')
+    console.log("Listening for motion and doorbell presses on your cameras.");
   }
 }
 
-example()
+example();
